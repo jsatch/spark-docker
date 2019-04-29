b0129fa5753d838af44a6f785c88e81b57b23252
Hernan Quintana <hernan@jsatch.com>
2019-04-28 23:27:59 -0500

    Images using ubuntu and openjdk8

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t spark-base:2.4.1 ./docker/base
-docker build -t spark-master:2.4.1 ./docker/spark-master
-docker build -t spark-worker:2.4.1 ./docker/spark-worker
-docker build -t spark-submit:2.4.1 ./docker/spark-submit
+docker build -t spark-base:2.4.2 ./docker/base
+docker build -t spark-master:2.4.2 ./docker/spark-master
+docker build -t spark-worker:2.4.2 ./docker/spark-worker
+docker build -t spark-submit:2.4.2 ./docker/spark-submit
